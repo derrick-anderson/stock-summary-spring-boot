98dabfbded3b0594d7f158147435e75c97db332c
Derrick Anderson <deanderson@solstice.com>
2018-08-03 11:30:43 -0500

    Fixes Spacing, Reorders Constructor

--- src/main/java/com/solstice/stocks/StockServices.java
+++ src/main/java/com/solstice/stocks/StockServices.java
@@ -12,55 +12,81 @@ import java.util.List;
 public class StockServices {
 
     private StockQuoteRepository repository;
-    public StockServices(StockQuoteRepository repository) {
-        this.repository = repository;
-    }
     private ObjectMapper mapper = new ObjectMapper();
+
+    public StockServices(StockQuoteRepository repository) {
+
+        this.repository = repository;
+
+    }
 
     public List<StockQuote> getAllStocks(){
 
         List<StockQuote> stock_list = null;
+
         try {
+
             stock_list = mapper.readValue(new URL("https://bootcamp-training-files.cfapps.io/week2/week2-stocks.json"), new TypeReference<List<StockQuote>>() {
             });
-        }catch( Exception e ){
+
+        }
+        catch( Exception e ){
+
             System.err.println(e.getMessage());
+
         }
         return stock_list;
     }
 
 
     public void loadStocks(){
+
         repository.saveAll(getAllStocks());
+
     }
 
 
     public String printStocks(List<StockQuote> stockQuotes) {
+
         try {
+
             return mapper.writeValueAsString(stockQuotes);
-        } catch (Exception e) {
+
+        }
+        catch (Exception e) {
+
             return "ISSUE";
+
         }
     }
 
 
     public StockSummary getSummary(String stock_in, String date_in){
+
         String[] dateSet = date_in.split("-");
-        if(dateSet.length == 3){
-            return repository.dailySummaryQuery(stock_in,dateSet[2], dateSet[1], dateSet[0]);
+
+        if(dateSet.length == 3) {
+
+            return repository.dailySummaryQuery(stock_in, dateSet[2], dateSet[1], dateSet[0]);
         }
         else if( dateSet.length == 2){
+
             return repository.monthlySummaryQuery(stock_in,dateSet[1], dateSet[0]);
+
         }
         else return null;
     }
 
 
     public String printSummary(StockSummary summary){
+
         try{
             return mapper.writeValueAsString(summary);
-        }catch (Exception e){
-            return "ISSUE";
         }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+            return null;
+        }
+
     }
 }
