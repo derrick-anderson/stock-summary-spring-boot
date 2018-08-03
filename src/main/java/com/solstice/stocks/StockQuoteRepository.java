649f0271b95a64cd1c441b17348d5a64108745d7
Derrick Anderson <deanderson@solstice.com>
2018-08-03 11:32:06 -0500

    Fixes Spacing, Removes unused method declarations.

--- src/main/java/com/solstice/stocks/StockQuoteRepository.java
+++ src/main/java/com/solstice/stocks/StockQuoteRepository.java
@@ -11,10 +11,6 @@ public interface StockQuoteRepository extends CrudRepository<StockQuote, Long> {
 
     List<StockQuote> findAllBySymbol(String symbol);
 
-    List<StockQuote> findAllBySymbolAndYearAndMonthAndDay(String symbol, String year, String month, String day);
-
-    List<StockQuote> findAllBySymbolAndYearAndMonth(String symbol, String year, String month);
-
     @Query(nativeQuery = true)
     StockSummary dailySummaryQuery(@Param("symbol_in") String symbol_in, @Param("day_in") String day_in,
                               @Param("month_in") String month_in, @Param("year_in") String year_in);
