153808fad696007e3cc6a37d567570d836f22cf2
Derrick Anderson <deanderson@solstice.com>
2018-08-06 16:41:22 -0500

    Cleaning up

--- src/main/java/com/solstice/stocks/data/StockSymbolRepository.java
+++ src/main/java/com/solstice/stocks/data/StockSymbolRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface StockSymbolRepository extends CrudRepository<StockSymbol, Long> {
