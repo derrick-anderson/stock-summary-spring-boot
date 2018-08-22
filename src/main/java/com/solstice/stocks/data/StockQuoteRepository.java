ba611fef963fdd2567af8ecbb17ffe603f126075
Derrick Anderson <deanderson@solstice.com>
2018-08-22 11:17:49 -0500

    Adds repository signature for get opening price and updates todos

--- src/main/java/com/solstice/stocks/data/StockQuoteRepository.java
+++ src/main/java/com/solstice/stocks/data/StockQuoteRepository.java
@@ -10,19 +10,22 @@ import java.util.List;
 @Repository
 public interface StockQuoteRepository extends JpaRepository<StockQuote, Long> {
 
+    //todo: Implement Query
     @Query("SELECT s FROM StockQuote s")
     List<StockQuote> getTotalVolumeForDate(String symbol, String dateIn, String dateFormat);
 
-    //todo: Create get high price
+    //todo: Implement Query
     @Query("SELECT s FROM StockQuote s")
     StockQuote getHighPriceForDate(String symbol, String dateIn, String dateFormat);
 
-    //todo: Create get low price
+    //todo: Implement Query
     @Query("SELECT s FROM StockQuote s")
     StockQuote getLowPriceForDate(String symbol, String dateIn, String dateFormat);
 
     //Stretch Goals
-    //todo: Create get open price
+    //todo: Implement Query
+    @Query("SELECT s FROM StockQuote s")
+    StockQuote getOpenPriceForDate(String symbol, String dateIn, String dateFormat)
 
     //todo: Create get closing price
 
