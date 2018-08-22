16c36386e3e7acb770f3db55ddbe3c2df1779f05
Derrick Anderson <deanderson@solstice.com>
2018-08-22 09:15:36 -0500

    Adds Mock test for get total volume call

---
+++ src/test/java/com/solstice/stocks/service/StockSummaryServicesUnitTests.java
@@ -0,0 +1,117 @@
+package com.solstice.stocks.service;
+
+
+import com.solstice.stocks.data.StockSymbolRepository;
+import com.solstice.stocks.model.StockQuote;
+import com.solstice.stocks.model.StockSummary;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class StockSummaryServicesUnitTests {
+
+    @Mock
+    private StockSymbolRepository stockSymbolRepository;
+
+    @InjectMocks
+    private StockServices stockServices;
+
+    @Before
+    public void setup() {
+
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    public void testDailySummaryReturn(){
+
+        StockSummary aSummary = new StockSummary();
+        aSummary.setSymbol("AAPL");
+        aSummary.setOpen_price(new BigDecimal(100.00));
+        aSummary.setHigh_price(new BigDecimal(150.00));
+        aSummary.setLow_price(new BigDecimal(50.00));
+        aSummary.setClosing_price(new BigDecimal(85.75));
+        aSummary.setVolume(525600);
+
+        when(stockSymbolRepository.dailySummaryQuery(any(String.class), any(int.class), any(int.class),any(int.class))).thenReturn(aSummary);
+
+        StockSummary newSummary = stockServices.getSummary("AAPL", "2018-06-22");
+
+        assertEquals("AAPL", newSummary.getSymbol());
+        assertEquals(new BigDecimal(100.00), newSummary.getOpen_price());
+        assertEquals(new BigDecimal(150.00), newSummary.getHigh_price());
+        assertEquals(new BigDecimal(50.00), newSummary.getLow_price());
+        assertEquals(new BigDecimal(85.75), newSummary.getClosing_price());
+        assertEquals(Integer.valueOf(525600), newSummary.getVolume());
+
+    }
+
+    @Test
+    public void testMonthlySummaryReturn(){
+        StockSummary aSummary = new StockSummary();
+        aSummary.setSymbol("AAPL");
+        aSummary.setOpen_price(new BigDecimal(100.00));
+        aSummary.setHigh_price(new BigDecimal(150.00));
+        aSummary.setLow_price(new BigDecimal(50.00));
+        aSummary.setClosing_price(new BigDecimal(85.75));
+        aSummary.setVolume(525600);
+
+        when(stockSymbolRepository.monthlySummaryQuery(any(String.class), any(int.class),any(int.class))).thenReturn(aSummary);
+
+        StockSummary newSummary = stockServices.getSummary("AAPL", "2018-06");
+
+        assertEquals("AAPL", newSummary.getSymbol());
+        assertEquals(new BigDecimal(100.00), newSummary.getOpen_price());
+        assertEquals(new BigDecimal(150.00), newSummary.getHigh_price());
+        assertEquals(new BigDecimal(50.00), newSummary.getLow_price());
+        assertEquals(new BigDecimal(85.75), newSummary.getClosing_price());
+        assertEquals(Integer.valueOf(525600), newSummary.getVolume());
+
+    }
+
+    @Test
+    public void testGetTotalVolumeForDate(){
+
+        List<StockQuote> quoteList = new ArrayList<StockQuote>();
+        StockQuote quote1 = new StockQuote();
+        quote1.setSymbol("AAPL");
+        quote1.setPrice(new BigDecimal(1500));
+        quote1.setVolume(150000);
+        quote1.setDate(new Date());
+        quoteList.add(quote1);
+
+        StockQuote quote2 = new StockQuote();
+        quote2.setSymbol("AAPL");
+        quote2.setPrice(new BigDecimal(1200));
+        quote2.setVolume(180000);
+        quote2.setDate(new Date());
+        quoteList.add(quote2);
+
+        when(stockSymbolRepository.getTotalVolumeForDate(any(), any(), any())).thenReturn(quoteList);
+
+        Integer totalVolume = stockServices.getTotalVolume(null, null, null);
+
+        assertEquals(Integer.valueOf(320000), totalVolume);
+    }
+
+
+
+
+}
