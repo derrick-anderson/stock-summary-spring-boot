20b0e2b80f0bb38af956790df9d175062ec9b1e4
Derrick Anderson <deanderson@solstice.com>
2018-08-23 11:41:40 -0500

    Spacing

--- src/test/java/com/solstice/stocks/data/StockQuoteRepositoryIntegrationTest.java
+++ src/test/java/com/solstice/stocks/data/StockQuoteRepositoryIntegrationTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
