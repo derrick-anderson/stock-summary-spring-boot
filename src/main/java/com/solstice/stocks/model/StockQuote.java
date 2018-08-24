fb33a1384d08f7fc21c41aecb8872803b15854a3
Derrick Anderson <deanderson@solstice.com>
2018-08-24 13:29:52 -0500

    Removes unused code

--- src/main/java/com/solstice/stocks/model/StockQuote.java
+++ src/main/java/com/solstice/stocks/model/StockQuote.java
@@ -1,12 +1,8 @@
 package com.solstice.stocks.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 @Entity
 @Table(name="stock_quotes")
@@ -19,16 +15,7 @@ public class StockQuote {
     private String symbol;
     private BigDecimal price;
     private int volume;
-
-    //@JsonFormat( shape= JsonFormat.Shape.STRING, pattern = "dd-MM-YYYY hh:mm:ss")
     private Date date;
-//    private int year;
-//    private int month;
-//    private int day;
-
-//    @ManyToOne
-//    @JoinColumn(name = "stock_symbol_id")
-//    private StockSymbol stockSymbol;
 
     public StockQuote() {
     }
@@ -74,54 +61,11 @@ public class StockQuote {
 
     public Date getDate() {  return date;  }
 
-//    public int getYear() {
-//        return year;
-//    }
-//
-//    public void setYear(int year) {
-//        this.year = year;
-//    }
-//
-//    public int getMonth() {
-//        return month;
-//    }
-//
-//    public void setMonth(int month) {
-//        this.month = month;
-//    }
-//
-//    public int getDay() {
-//        return day;
-//    }
-//
-//    public void setDay(int day) {
-//        this.day = day;
-//    }
-
-    //Adds custom date method writer
-
     public void setDate(Date date) {
 
         this.date = date;
 
-//        Calendar calendar = new GregorianCalendar();
-//        calendar.setTime(date);
-//
-//        this.year = calendar.get(Calendar.YEAR);
-//
-//        this.month = calendar.get(Calendar.MONTH)+1;
-//
-//        this.day = calendar.get(Calendar.DAY_OF_MONTH);
-
     }
-//
-//    public StockSymbol getStockSymbol() {
-//        return stockSymbol;
-//    }
-//
-//    public void setStockSymbol(StockSymbol stockSymbol) {
-//        this.stockSymbol = stockSymbol;
-//    }
 }
 
 
